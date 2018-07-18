ac8c962a9ba886f7dc330e306c50b55f881284e6
Ondřej Langr <langrik666@gmail.com>
2018-07-18 16:46:34 +0200

    User bugfix 1

--- app/CoreModule/presenters/UserPresenter.php
+++ app/CoreModule/presenters/UserPresenter.php
@@ -8,24 +8,28 @@
 
 namespace App\CoreModule\Presenters;
 
-
+use App\CoreModule\Model\Shop\Order\OrderManager;
 use App\Presenters\BasePresenter;
 use Nette\Database\Context;
+use Nette\Database\Table\ActiveRow;
 use Nette\Security\User;
 
 class UserPresenter extends BasePresenter
 {
     /** @var Context */
     private $context;
+    /** @var OrderManager  */
+    private $orderManager;
 
     /**
      * UserPresenter constructor.
      * @param Context $context
      */
-    public function __construct(Context $context)
+    public function __construct(Context $context, OrderManager $orderManager)
     {
         parent::__construct();
         $this->context = $context;
+        $this->orderManager = $orderManager;
     }
 
     /**
@@ -36,15 +40,15 @@ class UserPresenter extends BasePresenter
     {
         parent::startup();
         $id = (int) $this->presenter->getParameter('id');
-        if(($id && (int) $this->user->getId() !== $id) || $this->getUser()->isInRole('admin') == false)
+        bdump($id, 'id');
+        if(($id && (int) $this->user->getId() !== $id) && $this->getUser()->isInRole('admin') == false)
         {
+            bdump($id && (int) $this->user->getId() !== $id, 'firstCond');
             $this->error('You are not allowed to do this');
-            //$this->redirect('Product:list');
+            $this->redirect('Product:list');
 
         }elseif (!$id && $this->getUser()->isInRole('admin') == false) {
             $this->redirect('Auth:login');
-        }else{
-            $this->terminate();
         }
     }
 
@@ -55,11 +59,27 @@ class UserPresenter extends BasePresenter
     public function renderDefault(int $id = null)
     {
         if($id){
-            $user = $this->context->table('user')->select('email, name, picture, residence, phone, ico')->get($id);
+
+            $user = $this->context->table('user')->select('user_id, email, name, picture, residence, phone, ico')->where('user_id', $id)->get($id);
+            bdump($user);
             $orders = $user->related('order.user');
-            bdump($orders);
+
+            $orderArray = [];
+
+            foreach ($orders as $order) {
+                $orderArray[] = $this->orderManager->getOrder($order->id);
+                unset($order);
+            }
+            unset($orders);
+
+            foreach ($orderArray as $order){
+                $order->setProducts($this->orderManager->renderOrderProductFrontEnd($order));
+            }
+
+            bdump($orderArray, 'Orders');
+
             $this->template->user = $user;
-            $this->template->orders = $orders;
+            $this->template->orders = $orderArray;
         }else{
             $this->redirect('User:list');
         }
